3ce01d81d5531825108376674e2bd218334a9a4e
Amr Elsharkawy <31326736+AmrElsharkawi@users.noreply.github.com>
2020-10-10 21:08:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+       <html>
+       <body>
+
+       <?php
+       echo "My first PHP script! <br>" ;
+           
+       echo "hello world!";
+
+       ?>
+
+       </body>
+       </html>
